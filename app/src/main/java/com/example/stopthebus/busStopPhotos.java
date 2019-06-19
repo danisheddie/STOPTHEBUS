41bdc5a554993fe032f7904a9ce0c5e82c2bdbd9
danisheddie <danisheddie1405@gmail.com>
2019-06-19 14:10:23 +0800

    activity for bus stop photos

---
+++ app/src/main/java/com/example/stopthebus/busStopPhotos.java
@@ -0,0 +1,13 @@
+package com.example.stopthebus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class busStopPhotos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_stop_photos);
+    }
+}
